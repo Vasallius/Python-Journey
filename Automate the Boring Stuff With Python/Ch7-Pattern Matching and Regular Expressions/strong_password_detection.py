4c6ce9123f3316ad4d6ae8afb2f624e113c01224
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-21 21:51:47 +0800

    updated file

--- Automate the Boring Stuff With Python/Ch7-Pattern Matching and Regular Expressions/strong_password_detection.py
+++ Automate the Boring Stuff With Python/Ch7-Pattern Matching and Regular Expressions/strong_password_detection.py
@@ -8,23 +8,16 @@ capital_letters = re.compile(r'[A-Z]')
 lowercase_letters = re.compile(r'[a-z]')
 numbers = re.compile(r'[0-9]')
 
-if re.search(capital_letters,password) != None:
-    has_capital_letters = True
-else:
-    has_capital_letters = False
+has_capital_letters = True if re.search(capital_letters,password) != None else False
 
-if re.search(lowercase_letters,password) != None:
-    has_lowercase_letters = True
-else:
-    has_lowercase_letters = False
+has_lowercase_letters = True if re.search(lowercase_letters,password) != None else False
 
-if re.search(numbers,password) != None:
-    has_numbers = True
-else:
-    has_numbers = False
+has_numbers = True if re.search(numbers,password) != None else False
 
 if len(password) < 8:
     print("Password must be at least 8 characters long.")
 
 if has_capital_letters and has_lowercase_letters and has_numbers:
     print("Congrats! You have a strong password.")
+else: 
+    print("Weak password. Consider revising.")
