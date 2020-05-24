46a5c51246b32237c9ae05aad0a741cf08399d1c
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-24 16:40:11 +0800

    add file

---
+++ Automate the Boring Stuff With Python/Ch10-Organizing Files/select_copy.py
@@ -0,0 +1,24 @@
+# Vasallius
+
+import os
+import glob
+import shutil
+from pathlib import Path
+
+
+dir1 = Path(
+    r'C:\Users\Joseph\Desktop\dump')  # Replace path with directory containing jpg and png files
+
+total = 0
+
+for root, dir, file in os.walk(dir1):
+
+    for x in file:
+        # Check if file is a picture (jpg,png)
+        if ".jpg" in x or ".png" in x:
+            total += 1
+            imagepath = (f"{root}\\{x}")
+            # Replace path with your directory
+            shutil.copy(imagepath, r"E:\New folder (2)")
+
+print(f'{total} files copied.')
