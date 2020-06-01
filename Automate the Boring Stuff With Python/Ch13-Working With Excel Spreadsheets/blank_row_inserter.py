2948a54c92f60c8a7b19e95e4259b4ad0b1d7017
SubtleCoder7262 <masterjed7262@gmail.com>
2020-06-01 15:41:41 +0800

    add file

---
+++ Automate the Boring Stuff With Python/Ch13-Working With Excel Spreadsheets/blank_row_inserter.py
@@ -0,0 +1,22 @@
+# Vasallius
+
+# Import necessary modules
+import openpyxl
+import sys
+
+# Assign variables
+row = int(sys.argv[1])
+times = int(sys.argv[2])
+filename = sys.argv[3]
+
+# Open workbook
+wb = openpyxl.load_workbook(filename)
+sheet = wb.active
+
+# Insert rows
+while times != 0:
+    sheet.insert_rows(row)
+    times -= 1
+    row += 1
+
+wb.save(filename)
