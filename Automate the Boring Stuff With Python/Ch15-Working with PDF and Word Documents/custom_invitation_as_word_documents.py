218d1e7528e1b45d5af0c0e25e0fc78e267582d3
SubtleCoder7262 <masterjed7262@gmail.com>
2020-06-10 17:39:22 +0800

    add file

---
+++ Automate the Boring Stuff With Python/Ch15-Working with PDF and Word Documents/custom_invitation_as_word_documents.py
@@ -0,0 +1,30 @@
+# Vasallius
+
+# Import necessary module
+import docx
+
+# Open guests text file
+guests = open('guests.txt')
+guest_list = []
+
+# Add each guest to guest_list
+for line in guests.readlines():
+    line = line.rstrip()
+    guest_list.append(line)
+
+# Load the document with our styles
+doc = docx.Document('blank.docx')
+# Add the paragraph and set corresponding styles
+for index, guest in enumerate(guest_list):
+    doc.add_paragraph('It would be a pleasure to have the company of').style = 'inv-heading'
+    doc.add_paragraph(guest).style = 'name'
+    doc.add_paragraph('At 11010 Memory Lane on the Evening of').style = 'inv-heading'
+    doc.add_paragraph('April 1st').style = 'inv-date'
+    doc.add_paragraph('At 7 o’clock').style = 'inv-heading'
+    # Don't add page break for the last person
+    if index != len(guest_list)-1:
+        doc.paragraphs[len(doc.paragraphs)-1].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
+    else:
+        pass
+    
+doc.save('invitations.docx')
