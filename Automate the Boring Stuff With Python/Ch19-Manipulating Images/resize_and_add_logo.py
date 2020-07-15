a2c670a8e756141b641bd4054a8876e883346885
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-15 14:19:12 +0800

    add file

---
+++ Automate the Boring Stuff With Python/Ch19-Manipulating Images/resize_and_add_logo.py
@@ -0,0 +1,45 @@
+# Vasallius
+
+import os
+from PIL import Image
+
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = 'catlogo.png'
+
+logo = Image.open(LOGO_FILENAME)
+logoWidth, logoHeight = logo.size
+
+os.makedirs('withLogo', exist_ok=True)
+# Loop over all files in the working directory.
+for filename in os.listdir('.'):
+    if not (filename.lower().endswith('.png') or filename.lower().endswith('.jpg') or filename.lower().endswith('.gif') or filename.lower().endswith('.gif')) \
+       or filename == LOGO_FILENAME:
+        continue # skip non-image files and the logo file itself
+
+    im = Image.open(filename)
+    width, height = im.size
+
+    # Image must be at least twice bigger than logo to look good
+    if not width >= 2 * (logoWidth) and height >= 2 * (logoHeight):
+        continue
+
+    # Check if image needs to be resized.
+    if width > SQUARE_FIT_SIZE or height > SQUARE_FIT_SIZE:
+        # Calculate the new width and height to resize to.
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+
+        # Resize the image.
+        print(f'Resizing {filename}...')
+        im = im.resize((width, height))
+
+    # Add logo.
+    print(f'Adding logo to {filename}')
+    im.paste(logo, (width - logoWidth, height - logoHeight), logo)
+
+    # Save changes.
+    im.save(os.path.join('withLogo', filename))
