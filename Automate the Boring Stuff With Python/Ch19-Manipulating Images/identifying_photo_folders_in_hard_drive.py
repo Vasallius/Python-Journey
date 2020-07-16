2c5ec0e7f4045414251283facbea8b3c25b23b23
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-16 14:08:27 +0800

    add file

---
+++ Automate the Boring Stuff With Python/Ch19-Manipulating Images/identifying_photo_folders_in_hard_drive.py
@@ -0,0 +1,36 @@
+# Vasallius
+
+from PIL import Image
+import os
+
+for foldername, subfolders, filenames in os.walk('C:\\'):
+    numPhotoFiles = 0
+    numNonPhotoFiles = 0
+    for filename in filenames:
+        # Check if file extension isn't .png or .jpg.
+        if not filename.endswith('.jpg') or filename.endswith('.png'):
+            numNonPhotoFiles += 1
+            continue    # skip to next filename
+
+        # Open image file using Pillow.
+        try: 
+            img = Image.open(os.path.join(foldername,os.path.basename(filename)))
+            width, height = img.size
+        except:
+            print(f'Cannot open {filename} at {foldername}')
+
+        # Check if width & height are larger than 500.
+
+        if width > 500 and height > 500 :
+            # Image is large enough to be considered a photo.
+            numPhotoFiles += 1
+        else:
+            # Image is too small to be a photo.
+            numNonPhotoFiles += 1
+
+    # If more than half of files were photos,
+    # print the absolute path of the folder.
+
+    if numPhotoFiles > ((numNonPhotoFiles+numPhotoFiles)/2):
+        print(foldername)
+    
