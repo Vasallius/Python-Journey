966e526d50b0cf05b403c264721d58afa4f9e583
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-27 16:26:08 +0800

    refactored code and added comments

--- Automate the Boring Stuff With Python/Ch4-Lists/coin_flip_streaks.py
+++ Automate the Boring Stuff With Python/Ch4-Lists/coin_flip_streaks.py
@@ -1,20 +1,25 @@
+# Coin Flip Streaks
+
 import random
 
-numberOfStreaks = 0 #initialize variable to keep track of streaks
-masterList = []     #create an empty list
+number_of_streaks = 0  # Counter for streaks
+master_list = []
 
-for experimentNumber in range(10000):
-
-#Code that creates a list of 100 'heads' or 'tails' values.
+for experiment_number in range(10000):  # Perform experiment 10,000 times
 
     for i in range(100):
-        masterList.append(random.randint(0,1)) #0 and 1 to mimic head or tail values
-    print(masterList)                          #print list for confirmation
-    for x in range(94): #Used 94, to prevent IndexError
-        if masterList[x:x+6] == [1]*6 or masterList[x:x+6] == [0]*6:
-           numberOfStreaks += 1     #add 1 to numberOfStreaks variable
-           print('streak')
-           break
-    masterList = []                 #without this, masterList keeps on growing as new data is appended to it
+        if random.randint(0, 1) == 0:
+            master_list.append('H')
+        else:
+            master_list.append('T')
+    print(master_list)  # print list for visualization
+    for x in range(94):  # Used 94, to prevent IndexError
+        if master_list[x:x+6] == ['H'] * 6 or master_list[x:x+6] == ['T'] * 6:
+            number_of_streaks += 1
+            print('STREAK FOUND!')
+            break
+    master_list = []  # Without this, master_list keeps on growing as new data is appended to it
 
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+print('')
+print('Experimentation complete.')
+print(f'Chance of streak: {(number_of_streaks / 10000 * 100)}%')
