a59b8f594b1cd2215c566d9df2cd0e3b6d1b6d30
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-27 13:10:44 +0800

    added comments

--- Automate the Boring Stuff With Python/Ch3-Functions/input_validation.py
+++ Automate the Boring Stuff With Python/Ch3-Functions/input_validation.py
@@ -2,10 +2,10 @@
 
 print('Please enter an integer.')
 
-while True:
+while True:  # Loop until given an integer
     try:
         integer = int(input('> '))
         print('Thank you for entering an integer.')
         break
-    except ValueError:
+    except ValueError:  # Handle if user input is not an integer
         print('Error: please enter an integer')
