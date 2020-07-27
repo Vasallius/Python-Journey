fcacb33575818b993f7163c0f49216c0910add63
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-27 17:14:24 +0800

    added comments

--- Automate the Boring Stuff With Python/Ch4-Lists/character_picture_grid.py
+++ Automate the Boring Stuff With Python/Ch4-Lists/character_picture_grid.py
@@ -1,3 +1,5 @@
+# Character Picture Grid
+
 grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
@@ -11,9 +13,9 @@ grid = [['.', '.', '.', '.', '.', '.'],
 number_of_items_in_list = len(grid[0])
 number_of_lists = len(grid)
 
-print('this is my character grid \n')
+print('Here is the resulting character grid. \n')
 
 for i in range(number_of_items_in_list):
     for x in range(number_of_lists):
-        print (grid[x][i], end='')
+        print(grid[x][i], end='')
     print('')
