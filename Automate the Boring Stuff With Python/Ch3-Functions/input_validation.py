4cd3e51ff591fffb975fc9a4c058bb86caa9db38
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-27 13:16:37 +0800

    added a function validate; put extra comments

--- Automate the Boring Stuff With Python/Ch3-Functions/input_validation.py
+++ Automate the Boring Stuff With Python/Ch3-Functions/input_validation.py
@@ -2,10 +2,15 @@
 
 print('Please enter an integer.')
 
-while True:  # Loop until given an integer
-    try:
-        integer = int(input('> '))
-        print('Thank you for entering an integer.')
-        break
-    except ValueError:  # Handle if user input is not an integer
-        print('Error: please enter an integer')
+
+def validate():
+    while True:  # Loop until given an integer
+        try:
+            integer = int(input('> '))
+            print('Thank you for entering an integer.')
+            break  # Stop program when user enters an integer
+        except ValueError:  # Handle if user input is not an integer
+            print('Error: please enter an integer')
+
+
+validate()  # Calls the define 'validate' function above
