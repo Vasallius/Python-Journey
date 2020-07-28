dc3b7be6dd6a579c26884c2a842e6bb66dab7cce
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-28 14:00:00 +0800

    added comments and changed camel cased variables

--- Automate the Boring Stuff With Python/Ch6-Manipulating Strings/table_printer.py
+++ Automate the Boring Stuff With Python/Ch6-Manipulating Strings/table_printer.py
@@ -1,27 +1,23 @@
-# © Vasallius
+# Table Printer
 
-from builtins import list
-
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 length = []
-# append string lengths to lengthlist
-for list in tableData:
+
+# Append string lengths to length list
+for list in table_data:
     for word in list:
         length.append(len(word))
 
 longeststring = max(length)
 
 # print out table in organized manner
-
-
-def printTable(listoflists):
-    for x in range(4):
-        for y in range(3):
-            print(tableData[y][x].rjust(longeststring), end='')
+def print_table(listoflists):
+    for x in range(4): # Number of items in a list
+        for y in range(3): # Number of lists
+            print(table_data[y][x].rjust(longeststring), end='')
         print('')
 
-
-printTable(tableData)
+print_table(table_data)
