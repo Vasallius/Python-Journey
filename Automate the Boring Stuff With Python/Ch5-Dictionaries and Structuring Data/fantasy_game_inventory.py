73acbd9c9099d018bc5a10f1a93c2ad615403d1a
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-28 13:26:07 +0800

    added comments

--- Automate the Boring Stuff With Python/Ch5-Dictionaries and Structuring Data/fantasy_game_inventory.py
+++ Automate the Boring Stuff With Python/Ch5-Dictionaries and Structuring Data/fantasy_game_inventory.py
@@ -1,14 +1,15 @@
-# credits: Vasallius
+# Fantasy Game Inventory
 
 
-#initialize inventory dictionary
+# Initialize inventory dictionary
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 def display_inventory(inventoryname):
     totalitems= 0
+    # Print (key, value) pairs
     for k,v in inventoryname.items():
         print (v,k)
         totalitems += v
-    print('Total number of items: ' + str(totalitems) )
+    print('Total number of items: ' + str(totalitems))
 
 display_inventory(inventory)
