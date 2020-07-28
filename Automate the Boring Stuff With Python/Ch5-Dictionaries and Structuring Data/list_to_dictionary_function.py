0a85a13e9d6c842e3b66feb9070bcacb399b3495
SubtleCoder7262 <masterjed7262@gmail.com>
2020-07-28 13:39:35 +0800

    added comments and renamed camel case variables

--- Automate the Boring Stuff With Python/Ch5-Dictionaries and Structuring Data/list_to_dictionary_function.py
+++ Automate the Boring Stuff With Python/Ch5-Dictionaries and Structuring Data/list_to_dictionary_function.py
@@ -1,14 +1,17 @@
-#© Vasallius
+# List to Dictionary Function
 
+# Function taken from fantasy_game_inventory.py
 def display_inventory(inventoryname):
     totalitems= 0
     for k,v in inventoryname.items():
         print (v,k)
         totalitems += v
-    print('Total number of items: ' + str(totalitems) )
+    print('Total number of items: ' + str(totalitems))
 
-def addToInventory(inventory, addedItems):
-    for item in dragonLoot:
+# Takes in two dictionaries, first one being the inventory
+# Second one being items to be added to the inventory
+def add_to_inventory(inventory, added_items):
+    for item in added_items:
         inventory[item] = inventory.get(item,0)+1
     return inventory
 
@@ -16,5 +19,5 @@ inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 print('Inventory:')
-inv = addToInventory(inv, dragonLoot)
+inv = add_to_inventory(inv, dragonLoot)
 display_inventory(inv)
