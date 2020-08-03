7ac825d61be4468a6ee82b6d6a79cb83d1c13e1d
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-03 13:53:05 +0800

    refactored code

--- Automate the Boring Stuff With Python/Ch7-Pattern Matching and Regular Expressions/regex_strip.py
+++ Automate the Boring Stuff With Python/Ch7-Pattern Matching and Regular Expressions/regex_strip.py
@@ -1,39 +1,40 @@
-# Vasallius
+# Regex Version of the strip() Method
 
-'''This is the regex version of the strip() method'''
+'''
+Description:
+Write a function that takes a string and does the same thing as the strip() string method. 
+If no other arguments are passed other than the string to strip, 
+then whitespace characters will be removed from the beginning and end of the string. 
+Otherwise, the characters specified in the second argument to the function will be removed from the string.
+'''
 
-import re  # Import necesarry modules
+import re
 
-samplelst = []
+character_list = []
 
-# Ask user for input
 string_to_be_formatted = input("Enter string to be formatted: ")
 character_to_be_removed = input(
-    "Enter character to be stripped (pressing Enter removes whitespace left and right of string)")
+    "Enter character to be stripped (pressing Enter removes whitespace left and right of string): ")
 
-# Here, we set regex pattern to capture the nonwhitespace part of the string and store it in trimmed_text variable
+# (whitespace)(any)(whitespace)
 regex_pattern = re.compile(r'^(\s*)(.*?)(\s*)$')
-mo = re.search(regex_pattern, string_to_be_formatted)
-trimmed_text = mo.group(2)
-
-# Function that accepts two arguments, default second argument is whitespace symbol
+text = re.search(regex_pattern, string_to_be_formatted)
+trimmed_text = text.group(2)
 
 
 def regex_strip(string_to_be_formatted, character_to_be_removed):
     # If user presses enter, strip whitespace left and right of text
     if (len(character_to_be_removed) < 1):
         print(f"Output: {trimmed_text}")
+
     # If user enters a symbol, strip that symbol from the string
     else:
         for char in string_to_be_formatted:
-            samplelst.append(char)
-        # Remove symbol from the list of characters
-        while character_to_be_removed in samplelst:
-            samplelst.remove(character_to_be_removed)
-        # Join characters into a new list
+            character_list.append(char)
+        while character_to_be_removed in character_list:
+            character_list.remove(character_to_be_removed)
         final_list = "".join(samplelst)
-        print(f"Output:  {final_list}")
+        print(f"Output:{final_list}")
 
 
-# Execute function
 regex_strip(string_to_be_formatted, character_to_be_removed)
