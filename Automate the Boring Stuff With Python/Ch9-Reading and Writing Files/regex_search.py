f6dd88e90f8b112ef40667c53dfaa81be55359a0
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-03 21:33:42 +0800

    refactored code

--- Automate the Boring Stuff With Python/Ch9-Reading and Writing Files/regex_search.py
+++ Automate the Boring Stuff With Python/Ch9-Reading and Writing Files/regex_search.py
@@ -1,33 +1,36 @@
-# Vasallius
+# Regex Search
+
+'''
+Description: 
+Write a program that opens all .txt files in a folder and searches for any line 
+that matches a user-supplied regular expression. The results should be printed to the screen.
+'''
 
 import os
 import re
 
-# Initialize variables
 list_of_text_files = []
 list_of_matched_words = []
 
-# Get regex pattern from user
+
 pattern = input("Regex pattern to be searched: ")
 regexpattern = re.compile(pattern)
 
 # Gets all the paths of .txt files
 for filename in os.listdir(os.getcwd()):
-    filepath = os.path.join(os.getcwd(), filename)
-    if filepath.endswith('.txt',):
-        list_of_text_files.append(filepath)
+    if filename.endswith('.txt',):
+        list_of_text_files.append(filename)
 
-#Iterate over the paths and open
-for x in list_of_text_files:
-    fh = open(x)
-    for line in fh.readlines():
-        try:
-            matchedword = re.findall(regexpattern,line)
+# Iterate over the paths and open
+for filename in list_of_text_files:
+    with open(filename) as txt_file:
+        for line in txt_file.readlines():
+            try:
+                matchedwords = re.findall(regexpattern, line)
+            except AttributeError:
+                pass
+            # Add to list matched words
+            for matched_word in matchedwords:
+                list_of_matched_words.append(matched_word)
 
-        except AttributeError:
-            pass
-        #Add to list matched words
-        for x in matchedword:
-            list_of_matched_words.append(x)
-        
-print(list_of_matched_words)
+print(f'{len(list_of_matched_words)} matches found.\n{list_of_matched_words}')
