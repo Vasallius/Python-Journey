dcc47af75382cd5504a1b0bffe082dca8506e20a
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-04 13:36:20 +0800

    refactored code

---
+++ Automate the Boring Stuff With Python/Ch10-Organizing Files/selective_copy.py
@@ -0,0 +1,20 @@
+# Selective Copy
+
+import os
+import shutil
+
+source_directory = input('Enter the absolute path of the source folder: ')
+new_directory = input('Enter the absolute path of the destination folder: ')
+extension = input('Enter what type of file you want to copy (e.g. jpg): ')
+
+total_files = 0
+for root, dir, filename in os.walk(source_directory):
+
+    for document in filename:
+        # Check if file is a picture (.jpg, .png)
+        if document.endswith(extension):
+            total_files += 1
+            imagepath = (f"{root}\\{document}")
+            shutil.copy(imagepath, new_directory)
+
+print(f'{total_files} files copied to {new_directory}.')
