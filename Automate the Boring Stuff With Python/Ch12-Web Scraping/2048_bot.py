e7946ecab4bee72be1da49817bbcc60e91901ce6
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-05 13:32:33 +0800

    refactored code

--- Automate the Boring Stuff With Python/Ch12-Web Scraping/2048_bot.py
+++ Automate the Boring Stuff With Python/Ch12-Web Scraping/2048_bot.py
@@ -1,4 +1,4 @@
-# Vasallius
+# 2048 Bot
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 browser.get('https://gabrielecirulli.github.io/2048/')
 gameover = False
-verify = []
 whole_page_elem = browser.find_element_by_tag_name('html')
 
 while gameover == False:
@@ -18,10 +17,9 @@ while gameover == False:
     gameovertext = browser.find_elements_by_xpath(
         '//div[@class="game-message game-over"]/p')
     if len(gameovertext) == 1:
-        verify.append(gameovertext)
-    if len(verify) > 0:
         gameover = True
+
 
 if gameover == True:
     score = browser.find_element_by_class_name('score-container')
-    print(f"Game over, you score was {score.text}")
+    print(f"Game over, you score was {score.text}.")
