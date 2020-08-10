1a38e5c9389fd7468bf62cf1806bc580d991bb5e
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-10 13:06:39 +0800

    refactored code

--- Automate the Boring Stuff With Python/Ch15-Working with PDF and Word Documents/custom_invitation_as_word_documents.py
+++ Automate the Boring Stuff With Python/Ch15-Working with PDF and Word Documents/custom_invitation_as_word_documents.py
@@ -1,6 +1,5 @@
-# Vasallius
+# Custom Invitation as Word Documents
 
-# Import necessary module
 import docx
 
 # Open guests text file
@@ -13,18 +12,21 @@ for line in guests.readlines():
     guest_list.append(line)
 
 # Load the document with our styles
-doc = docx.Document('blank.docx')
+doc = docx.Document('style.docx')
 # Add the paragraph and set corresponding styles
 for index, guest in enumerate(guest_list):
-    doc.add_paragraph('It would be a pleasure to have the company of').style = 'inv-heading'
+    doc.add_paragraph(
+        'It would be a pleasure to have the company of').style = 'inv-heading'
     doc.add_paragraph(guest).style = 'name'
-    doc.add_paragraph('At 11010 Memory Lane on the Evening of').style = 'inv-heading'
+    doc.add_paragraph(
+        'At 11010 Memory Lane on the Evening of').style = 'inv-heading'
     doc.add_paragraph('April 1st').style = 'inv-date'
     doc.add_paragraph('At 7 o’clock').style = 'inv-heading'
     # Don't add page break for the last person
     if index != len(guest_list)-1:
-        doc.paragraphs[len(doc.paragraphs)-1].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
+        doc.paragraphs[len(doc.paragraphs) -
+                       1].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
     else:
         pass
-    
+
 doc.save('invitations.docx')
