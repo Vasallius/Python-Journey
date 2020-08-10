19a3c69c4cc0fdb822bf69d08c941fd35af8a860
SubtleCoder7262 <masterjed7262@gmail.com>
2020-08-10 13:20:04 +0800

    changed comment header

--- Automate the Boring Stuff With Python/Ch16-Working with CSV Files and JSON Data/excel_to_csv_converter.py
+++ Automate the Boring Stuff With Python/Ch16-Working with CSV Files and JSON Data/excel_to_csv_converter.py
@@ -1,14 +1,15 @@
-# Vasallius
+# Excel to CSV Converter
 
-# Import necessary modules
-import openpyxl, csv, os
+import openpyxl
+import csv
+import os
 
 for excel_file in os.listdir('.'):
     # Skip non-excel files
     if not excel_file.endswith('.xlsx'):
         continue
 
-    #Load excel file
+    # Load excel file
     wb = openpyxl.load_workbook(excel_file)
 
     # Loop through every sheet in the workbook.
@@ -16,19 +17,19 @@ for excel_file in os.listdir('.'):
         sheet = wb[sheetname]
 
         # Create the CSV filename from the Excel filename and sheet title.
-        csvFile = open(f'{excel_file}_{sheetname}.csv', 'w',newline='')
+        csvFile = open(f'{excel_file}_{sheetname}.csv', 'w', newline='')
 
         # Create the csv.writer object for this CSV file.
         outputWriter = csv.writer(csvFile)
 
         # Loop through every row in the sheet.
         for rowNum in range(1, sheet.max_row + 1):
-            rowData = []   
+            rowData = []
 
             # Loop through each cell in the row.
             for colNum in range(1, sheet.max_column + 1):
                 # Append each cell's data to rowData.
-                rowData.append(sheet.cell(row=rowNum,column=colNum).value)
+                rowData.append(sheet.cell(row=rowNum, column=colNum).value)
             # Write the rowData list to the CSV file.
             outputWriter.writerow(rowData)
         csvFile.close()
